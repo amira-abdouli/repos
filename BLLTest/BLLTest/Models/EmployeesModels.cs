29d918e709c368f59aa83e15acc8b14438b01eb9
Amira Abdouli <a.abdouli@root.sa>
2018-03-08 11:32:30 +0100

    Add project files.

---
+++ BLLTest/BLLTest/Models/EmployeesModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLLTest.Models
+{
+    public class EmployeesModels
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+}
